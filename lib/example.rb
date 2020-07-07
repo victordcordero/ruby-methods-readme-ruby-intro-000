ba1317e51a5e682cf2279e205ccff7acdf1aa50b
Victor Cordero <victordcordero@gmail.com>
2020-07-07 00:39:02 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,1 @@
 # Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
